3df15564ee8228e6917eebd5e03179deece61ff0
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-08-04 09:36:12 +0200

    Added key reference to models

--- FriendlyGames.Domain/Models/User.cs
+++ FriendlyGames.Domain/Models/User.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FriendlyGames.Domain.Models;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
