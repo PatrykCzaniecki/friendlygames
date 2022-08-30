a90a1889cfb05dbb59ae32b6fa0b91b85eb7629d
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-08-30 15:12:55 +0200

    Seed sample data

--- FriendlyGames.DataAccess/FriendlyGamesDbContext.cs
+++ FriendlyGames.DataAccess/FriendlyGamesDbContext.cs
@@ -39,35 +39,72 @@ public class FriendlyGamesDbContext : DbContext
             .OnDelete(DeleteBehavior.Restrict);
 
         // Seeding data
-        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 1, Name = "Waiting"});
-        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 2, Name = "Accepted"});
-        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 3, Name = "Rejected"});
-        modelBuilder.Entity<SurroundingCategory>().HasData(new SurroundingCategory {Id = 1, Name = "Indoor"});
-        modelBuilder.Entity<SurroundingCategory>().HasData(new SurroundingCategory {Id = 2, Name = "Outdoor"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 1, Name = "Grass"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 2, Name = "Court"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 3, Name = "Sand"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 4, Name = "Hall"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 5, Name = "Pool"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 6, Name = "Synthetic"});
-        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 7, Name = "Others"});
-        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 1, Name = "Easy"});
-        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 2, Name = "Medium"});
-        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 3, Name = "Advanced"});
+        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 1, Name = "Oczekujące"});
+        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 2, Name = "Zaakceptowane"});
+        modelBuilder.Entity<RegistrationCategory>().HasData(new RegistrationCategory {Id = 3, Name = "Odrzucone"});
+        modelBuilder.Entity<SurroundingCategory>().HasData(new SurroundingCategory {Id = 1, Name = "Węwnątrz"});
+        modelBuilder.Entity<SurroundingCategory>().HasData(new SurroundingCategory {Id = 2, Name = "Na zewnątrz"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 1, Name = "Trawa"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 2, Name = "Kort"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 3, Name = "Piasek"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 4, Name = "Hala"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 5, Name = "Basen"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 6, Name = "Syntetyczna"});
+        modelBuilder.Entity<SurfaceCategory>().HasData(new SurfaceCategory {Id = 7, Name = "Inne"});
+        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 1, Name = "Łatwy"});
+        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 2, Name = "Średni"});
+        modelBuilder.Entity<LevelCategory>().HasData(new LevelCategory {Id = 3, Name = "Zaawanzowany"});
         modelBuilder.Entity<Location>().HasData(new Location
             { Id = 1, City = "Tarnów", Street = "Piłsudskiego 24", EventId = 1 });
         modelBuilder.Entity<Location>().HasData(new Location
             { Id = 2, City = "Kraków", Street = "Grzegórzecka 24", EventId = 2 });
+        modelBuilder.Entity<Location>().HasData(new Location
+            { Id = 3, City = "Żywiec", Street = "Kazimierza Tetmajera 75", EventId = 3 });
+        modelBuilder.Entity<Location>().HasData(new Location
+            { Id = 4, City = "Wrocław", Street = "Różanka", EventId = 4 });
+        modelBuilder.Entity<Location>().HasData(new Location
+            { Id = 5, City = "Szczecin", Street = "Modra 104", EventId = 5 });
+        modelBuilder.Entity<Location>().HasData(new Location
+            { Id = 6, City = "Warszawa", Street = "Vincenta van Gogha 1", EventId = 6 });
         modelBuilder.Entity<EventCategory>()
             .HasData(new EventCategory
                 {Id = 1, Name = "Koszykówka", Description = "Szukam osób do gry w kosza"});
         modelBuilder.Entity<EventCategory>()
             .HasData(new EventCategory
                 {Id = 2, Name = "Piłka Nożna", Description = "Orlikowe granie"});
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 3, Name = "Siłownia", Description = "Ciężki trening" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 4, Name = "Bieganie", Description = "Sprinty na 200m" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 5, Name = "Rower", Description = "Nauka jazdy na jednym kole" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 6, Name = "Siatkówka", Description = "Mecz o wszystko" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 7, Name = "Tenis", Description = "1 vs 1" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 8, Name = "Ping Pong", Description = "Brak stołu.., ktoś coś?" });
+        modelBuilder.Entity<EventCategory>()
+            .HasData(new EventCategory
+                { Id = 9, Name = "Kręgielnia", Description = "Sobotni chill" });
         modelBuilder.Entity<User>()
             .HasData(new User {Id = 1, FirstName = "John", LastName = "Doe"});
         modelBuilder.Entity<User>()
             .HasData(new User {Id = 2, FirstName = "Adam", LastName = "Smith"});
+        modelBuilder.Entity<User>()
+            .HasData(new User { Id = 3, FirstName = "Franek", LastName = "Stopka" });
+        modelBuilder.Entity<User>()
+            .HasData(new User { Id = 4, FirstName = "Asia", LastName = "Szul" });
+        modelBuilder.Entity<User>()
+            .HasData(new User { Id = 5, FirstName = "Tomek", LastName = "Broda" });
+        modelBuilder.Entity<User>()
+            .HasData(new User { Id = 6, FirstName = "Grzegorz", LastName = "Wisła" });
         modelBuilder.Entity<Event>()
             .HasData(new Event
             {
@@ -102,6 +139,74 @@ public class FriendlyGamesDbContext : DbContext
                 LocationId = 2,
                 ImageForEvent = "footbal-box.png"
             });
+        modelBuilder.Entity<Event>()
+            .HasData(new Event
+            {
+                Id = 3,
+                Name = "Siłownia",
+                CreatorId = 3,
+                StartDateTime = new DateTime(2022, 8, 1, 12, 0, 0),
+                EndDateTime = new DateTime(2022, 8, 1, 14, 0, 0),
+                EventCategoryId = 3,
+                LevelCategoryId = 2,
+                SurfaceCategoryId = 3,
+                SurroundingCategoryId = 2,
+                MaxNumberOfPlayers = 10,
+                PriceForEvent = 0.0,
+                LocationId = 3,
+                ImageForEvent = "footbal-box.png"
+            });
+        modelBuilder.Entity<Event>()
+            .HasData(new Event
+            {
+                Id = 4,
+                Name = "Bieganie",
+                CreatorId = 4,
+                StartDateTime = new DateTime(2022, 8, 1, 12, 0, 0),
+                EndDateTime = new DateTime(2022, 8, 1, 14, 0, 0),
+                EventCategoryId = 4,
+                LevelCategoryId = 3,
+                SurfaceCategoryId = 7,
+                SurroundingCategoryId = 2,
+                MaxNumberOfPlayers = 3,
+                PriceForEvent = 0.0,
+                LocationId = 4,
+                ImageForEvent = "footbal-box.png"
+            });
+        modelBuilder.Entity<Event>()
+            .HasData(new Event
+            {
+                Id = 5,
+                Name = "Rower",
+                CreatorId = 5,
+                StartDateTime = new DateTime(2022, 8, 1, 12, 0, 0),
+                EndDateTime = new DateTime(2022, 8, 1, 14, 0, 0),
+                EventCategoryId = 5,
+                LevelCategoryId = 2,
+                SurfaceCategoryId = 7,
+                SurroundingCategoryId = 2,
+                MaxNumberOfPlayers = 15,
+                PriceForEvent = 10.0,
+                LocationId = 5,
+                ImageForEvent = "footbal-box.png"
+            });
+        modelBuilder.Entity<Event>()
+            .HasData(new Event
+            {
+                Id = 6,
+                Name = "Kręgle",
+                CreatorId = 6,
+                StartDateTime = new DateTime(2022, 8, 1, 12, 0, 0),
+                EndDateTime = new DateTime(2022, 8, 1, 14, 0, 0),
+                EventCategoryId = 9,
+                LevelCategoryId = 1,
+                SurfaceCategoryId = 4,
+                SurroundingCategoryId = 1,
+                MaxNumberOfPlayers = 4,
+                PriceForEvent = 16.0,
+                LocationId = 6,
+                ImageForEvent = "footbal-box.png"
+            });
         modelBuilder.Entity<Registration>()
             .HasData(new Registration
             {
@@ -129,6 +234,34 @@ public class FriendlyGamesDbContext : DbContext
                 EventId = 2,
                 UserId = 1,
                 RegistrationCategoryId = 3
+            });
+        modelBuilder.Entity<Registration>()
+            .HasData(new Registration
+            {
+                EventId = 3,
+                UserId = 3,
+                RegistrationCategoryId = 2
+            });
+        modelBuilder.Entity<Registration>()
+            .HasData(new Registration
+            {
+                EventId = 4,
+                UserId = 4,
+                RegistrationCategoryId = 2
+            });
+        modelBuilder.Entity<Registration>()
+            .HasData(new Registration
+            {
+                EventId = 5,
+                UserId = 5,
+                RegistrationCategoryId = 2
+            });
+        modelBuilder.Entity<Registration>()
+            .HasData(new Registration
+            {
+                EventId = 6,
+                UserId = 6,
+                RegistrationCategoryId = 2
             });
     }
 }
---
+++ FriendlyGames.DataAccess/Migrations/20220830131026_SeedingData.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FriendlyGames.DataAccess.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "EventCategories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 3, "Ciężki trening", "Siłownia" },
+                    { 4, "Sprinty na 200m", "Bieganie" },
+                    { 5, "Nauka jazdy na jednym kole", "Rower" },
+                    { 6, "Mecz o wszystko", "Siatkówka" },
+                    { 7, "1 vs 1", "Tenis" },
+                    { 8, "Brak stołu.., ktoś coś?", "Ping Pong" },
+                    { 9, "Sobotni chill", "Kręgielnia" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Łatwy");
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Średni");
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Zaawanzowany");
+
+            migrationBuilder.InsertData(
+                table: "Location",
+                columns: new[] { "Id", "City", "EventId", "Street" },
+                values: new object[,]
+                {
+                    { 3, "Żywiec", 3, "Kazimierza Tetmajera 75" },
+                    { 4, "Wrocław", 4, "Różanka" },
+                    { 5, "Szczecin", 5, "Modra 104" },
+                    { 6, "Warszawa", 6, "Vincenta van Gogha 1" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Oczekujące");
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Zaakceptowane");
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Odrzucone");
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 1, 1 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9086));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 1, 2 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9135));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 2, 1 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9155));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 2, 2 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9146));
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Trawa");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Kort");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Piasek");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Hala");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Basen");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Syntetyczna");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Name",
+                value: "Inne");
+
+            migrationBuilder.UpdateData(
+                table: "SurroundingCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Węwnątrz");
+
+            migrationBuilder.UpdateData(
+                table: "SurroundingCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Na zewnątrz");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 3, "Franek", "Stopka" },
+                    { 4, "Asia", "Szul" },
+                    { 5, "Tomek", "Broda" },
+                    { 6, "Grzegorz", "Wisła" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "CreatorId", "EndDateTime", "EventCategoryId", "ImageForEvent", "LevelCategoryId", "LocationId", "MaxNumberOfPlayers", "Name", "PriceForEvent", "StartDateTime", "SurfaceCategoryId", "SurroundingCategoryId" },
+                values: new object[,]
+                {
+                    { 3, 3, new DateTime(2022, 8, 1, 14, 0, 0, 0, DateTimeKind.Unspecified), 3, "footbal-box.png", 2, 3, 10, "Siłownia", 0.0, new DateTime(2022, 8, 1, 12, 0, 0, 0, DateTimeKind.Unspecified), 3, 2 },
+                    { 4, 4, new DateTime(2022, 8, 1, 14, 0, 0, 0, DateTimeKind.Unspecified), 4, "footbal-box.png", 3, 4, 3, "Bieganie", 0.0, new DateTime(2022, 8, 1, 12, 0, 0, 0, DateTimeKind.Unspecified), 7, 2 },
+                    { 5, 5, new DateTime(2022, 8, 1, 14, 0, 0, 0, DateTimeKind.Unspecified), 5, "footbal-box.png", 2, 5, 15, "Rower", 10.0, new DateTime(2022, 8, 1, 12, 0, 0, 0, DateTimeKind.Unspecified), 7, 2 },
+                    { 6, 6, new DateTime(2022, 8, 1, 14, 0, 0, 0, DateTimeKind.Unspecified), 9, "footbal-box.png", 1, 6, 4, "Kręgle", 16.0, new DateTime(2022, 8, 1, 12, 0, 0, 0, DateTimeKind.Unspecified), 4, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Registrations",
+                columns: new[] { "EventId", "UserId", "RegistrationCategoryId", "RegistrationDateTime" },
+                values: new object[,]
+                {
+                    { 3, 3, 2, new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9165) },
+                    { 4, 4, 2, new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9175) },
+                    { 5, 5, 2, new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9185) },
+                    { 6, 6, 2, new DateTime(2022, 8, 30, 15, 10, 26, 138, DateTimeKind.Local).AddTicks(9194) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 6, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "EventCategories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Easy");
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Medium");
+
+            migrationBuilder.UpdateData(
+                table: "LevelCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Advanced");
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Waiting");
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Accepted");
+
+            migrationBuilder.UpdateData(
+                table: "RegistrationCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Rejected");
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 1, 1 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 11, 16, 21, 350, DateTimeKind.Local).AddTicks(1386));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 1, 2 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 11, 16, 21, 350, DateTimeKind.Local).AddTicks(1587));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 2, 1 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 11, 16, 21, 350, DateTimeKind.Local).AddTicks(1645));
+
+            migrationBuilder.UpdateData(
+                table: "Registrations",
+                keyColumns: new[] { "EventId", "UserId" },
+                keyValues: new object[] { 2, 2 },
+                column: "RegistrationDateTime",
+                value: new DateTime(2022, 8, 30, 11, 16, 21, 350, DateTimeKind.Local).AddTicks(1619));
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Grass");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Court");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Sand");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Hall");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Pool");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Synthetic");
+
+            migrationBuilder.UpdateData(
+                table: "SurfaceCategories",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Name",
+                value: "Others");
+
+            migrationBuilder.UpdateData(
+                table: "SurroundingCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Indoor");
+
+            migrationBuilder.UpdateData(
+                table: "SurroundingCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Outdoor");
+        }
+    }
+}
