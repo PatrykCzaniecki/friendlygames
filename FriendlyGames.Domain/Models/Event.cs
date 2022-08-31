a5599f4fe5dde162177d1971361c4684b294f736
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-08-31 14:12:01 +0200

    edit

--- FriendlyGames.Domain/Models/Event.cs
+++ FriendlyGames.Domain/Models/Event.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
 using FriendlyGames.Domain.Categories;
 using FriendlyGames.Domain.Enums;
 
