2b07c865dc7b4cf8e89bdce3de267f62f0703674
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-09-12 11:54:59 +0200

    Merge

---
+++ FriendlyGames.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using FriendlyGames.Api.Dtos;
+using FriendlyGames.DataAccess;
+using FriendlyGames.Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendlyGames.Api.Controllers.CategoryControllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly FriendlyGamesDbContext _dbContext;
+        private readonly ILogger<CategoriesController> _logger;
+        private readonly IMapper _mapper;
+
+
+        public CategoriesController(IMapper mapper, ILogger<CategoriesController> logger, FriendlyGamesDbContext dbContext)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Dictionary<string, object>>> GetCategories()
+        {
+            _logger.LogInformation($"{nameof(GetCategories)} called...");
+
+            try
+            {
+                var eventCategories = await _dbContext.EventCategories.ToListAsync();
+                var levelCategories = await _dbContext.LevelCategories.ToListAsync();
+                var surfaceCategories = await _dbContext.SurfaceCategories.ToListAsync();
+                var surroundingCategories = await _dbContext.SurroundingCategories.ToListAsync();
+
+                var dictionary = new Dictionary<string, object>
+                {
+                    {
+                        "eventCategory", eventCategories
+                    },
+                    {
+                        "levelCategory", levelCategories
+                    },
+                    {
+                        "surfaceCategory", surfaceCategories
+                    },
+                    {
+                        "surroundingCategory", surroundingCategories
+                    }
+                };
+
+                return Ok(dictionary);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Something went wrong in {nameof(GetCategories)}");
+
+                return Problem("Internal server error, please try again later...");
+            }
+
+
+        }
+    }
+}
--- FriendlyGames.Api/Dtos/SurroundingsDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FriendlyGames.Api.Dtos
-{
-    public class SurroundingsDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
